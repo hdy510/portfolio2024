29eead9a490d2f2cefe0b9f75888fbd83c1f1d68
hdy510 <hdy510@naver.com>
2024-10-28 20:07:19 +0900

    241028-main js 시작

--- project4/js.js
+++ project4/js.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
-
-
-
+    // initialize swiper
+    var swiper = new Swiper(".heroSwiper", {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
 
 
 
